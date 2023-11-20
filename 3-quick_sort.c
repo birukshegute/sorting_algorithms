c972202e881dcce253e78973cb248e2370651a45
birushegute <birushegute@gmail.com>
2023-11-20 13:34:39 -0800

    selection sort complete

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * quick_sort - function to do quick sort algorithm
+ * @array: array to be sorted
+ * @size - size of the array
+ *
+ * Return - always 0
+ */
+void quick_sort(int *array, size_t size)
+{
+
