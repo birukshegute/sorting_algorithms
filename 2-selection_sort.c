603cff6109eb0d4ba654cdb873b407214b566ed5
birushegute <birushegute@gmail.com>
2023-11-20 13:27:20 -0800

    selection sort complete?

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -33,11 +33,11 @@ void selection_sort(int *array, size_t size)
 			{
 				min = j;
 			}
-			if (min != i)
-			{
-				swap(&array[min], &array[i]);
-			}
 		}
-	 print_array(array, size);
+		if (min != i)
+		{
+			swap(&array[min], &array[i]);
+			print_array(array, size);
+		}
 	}
 }
