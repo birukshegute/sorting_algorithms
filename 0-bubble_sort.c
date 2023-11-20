cc13cf3a67113e37186ae51ec313509e3ed9b2bb
birushegute <birushegute@gmail.com>
2023-11-20 04:23:48 -0800

    bubble sort almost done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,17 +8,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int *temp;
+	size_t i, j; 
+	    
+	int temp;
 
-	for(int i = 0, i < size, i++)
+	for(i = 0; i < size; i++)
 	{
-		for(int j = 0, j < (size - 1), j++)
+		for(j = 0; j < (size - 1); j++)
 		{
-			if(array(j) > array(j+1))
+			if(array[j] > array[j+1])
 			{
-				temp = array(j);
-				array(j) = array(j+1);
-				array(j+1) = temp;
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
 				print_array(array, size);
 			}
 		}
--- sort.h
+++ sort.h
@@ -1,10 +1,11 @@
+#include <stddef.h>
 #ifndef SORT_H
 #define SORT_H
-	
-void print_array(const int *array, size_t size)
-void print_list(const listint_t *list)
+
+extern size_t size;
+
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
